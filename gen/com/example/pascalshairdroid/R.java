8850d3f33b9f502998f37a97e48cb43df9c82ac9
oli9k9 <oli9k9@hotmail.com>
2014-11-28 07:00:37 +0100

    Erste Layoutfiles hinzugefügt. Strings aus res/layout/activity_main.xml nach res/values/strings.xml exportiert.

--- gen/com/example/pascalshairdroid/R.java
+++ gen/com/example/pascalshairdroid/R.java
@@ -26,14 +26,17 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
-        public static final int button1=0x7f080003;
+        public static final int button1=0x7f080004;
         public static final int editText1=0x7f080000;
         public static final int editText2=0x7f080001;
         public static final int editText3=0x7f080002;
-        public static final int textView1=0x7f080004;
+        public static final int textView1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int home=0x7f030001;
+        public static final int register=0x7f030002;
+        public static final int reservation=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -42,6 +45,11 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int nachname=0x7f050004;
+        public static final int passwort=0x7f050005;
+        public static final int registrierung=0x7f050006;
+        public static final int vorname=0x7f050003;
+        public static final int weiter=0x7f050007;
     }
     public static final class style {
         /** 
