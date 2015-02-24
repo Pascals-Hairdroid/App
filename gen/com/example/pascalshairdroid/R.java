bfb704de8b3b7407c9cf5d852720dd0d0c3061e1
Thomas <Thomas@Thomas-PC>
2015-02-24 10:44:54 +0100

    App Logo geändert

--- gen/com/example/pascalshairdroid/R.java
+++ gen/com/example/pascalshairdroid/R.java
@@ -31,12 +31,13 @@ public final class R {
         public static final int ic_drawer=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int ph=0x7f020003;
-        public static final int team_foto9=0x7f020004;
+        public static final int ph1=0x7f020004;
+        public static final int team_foto9=0x7f020005;
     }
     public static final class id {
         public static final int action_example=0x7f090013;
         public static final int action_settings=0x7f090012;
-        public static final int adresse=0x7f09000e;
+        public static final int adresse=0x7f09000c;
         public static final int b_registierung=0x7f090004;
         public static final int company_name=0x7f09000b;
         public static final int container=0x7f090001;
@@ -46,7 +47,7 @@ public final class R {
         public static final int navigation_drawer=0x7f090002;
         public static final int opentime_webview=0x7f09000f;
         public static final int ort=0x7f09000d;
-        public static final int petritsch=0x7f09000c;
+        public static final int petritsch=0x7f09000e;
         public static final int section_label=0x7f09000a;
         public static final int t_nachname=0x7f090007;
         public static final int t_passwort=0x7f090006;
