289294c96121be04e10f9369a8ccac42c31b85b2
Thomas <Thomas@10.32.7.159>
2015-03-06 12:47:13 +0100

    Link verändert

--- src/com/example/pascalshairdroid/FriseurstudioFragment.java
+++ src/com/example/pascalshairdroid/FriseurstudioFragment.java
@@ -33,7 +33,7 @@ public class FriseurstudioFragment extends Fragment {
 					return false;
 				}
 			});
-			view.loadUrl("http://google.com?id=");
+			view.loadUrl("http://myfirsttrysodontblameme.ddns.net/Homepage/Homepage");
 			
 			
 			return rootView;
