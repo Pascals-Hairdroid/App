ee8dd95a71d06088b852d6fb1bdccf19486b654a
Thomas Kesman <kes15779@spengergasse.at>
2015-06-23 22:22:51 +0200

    Galerie Bilder geändert;;;

--- src/galerie/AppConstantGalerie.java
+++ src/galerie/AppConstantGalerie.java
@@ -18,7 +18,7 @@ public class AppConstantGalerie {
 	public static final ArrayList<Integer> images = new ArrayList<Integer>(
 			Arrays.asList(new Integer[] { R.drawable.galerie1,
 					R.drawable.galerie2,R.drawable.galerie3,R.drawable.galerie4,R.drawable.galerie5,R.drawable.galerie6,R.drawable.galerie7,
-					R.drawable.galerie8,R.drawable.galerie10,R.drawable.galerie11,R.drawable.galerie12,R.drawable.galerie13,
+					R.drawable.galerie8, R.drawable.galerie9, R.drawable.galerie10,R.drawable.galerie11,R.drawable.galerie12,R.drawable.galerie13,
 					R.drawable.galerie14,R.drawable.galerie15,R.drawable.galerie16,R.drawable.galerie17, R.drawable.galerie18,
 					R.drawable.galerie19,R.drawable.galerie20,R.drawable.galerie21, R.drawable.galerie22, R.drawable.galerie23, R.drawable.galerie24}));
 }
