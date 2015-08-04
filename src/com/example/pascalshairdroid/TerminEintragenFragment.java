3e98476c5b0dd359abd92d454ff4d282e89b110f
oli9k9 <oli9k9@hotmail.com>
2015-08-04 19:13:57 +0200

    Relogin in TerminEintragen implementiert;

--- src/com/example/pascalshairdroid/TerminEintragenFragment.java
+++ src/com/example/pascalshairdroid/TerminEintragenFragment.java
@@ -60,7 +60,7 @@ public class TerminEintragenFragment extends Fragment {
 			// Überprüfung ob Internet vorhanden 
 			// Wenn keine Internet verbindung HTML Seite no Internet Connection anzeigen 
 			if (Utils.isInternetAvailable(getActivity())) {
-				view.loadUrl("http://pascals.at/v2/Seiten/terminvergabe.php?web=1");
+				view.loadUrl("http://pascals.at/v2/Seiten/terminvergabe.php?web=1&sessionId="+sessionID);
 			} else {
 				//fallback seite anzeigen falls offline
 				view.loadData("<html><head><style>body{background-color:lightgray;margin-top:25%;}h2{color:orange;text-align:center;Font-Family:Calibri;}</style><title></title></head><body><h2>Es konnte keine Internetverbindung hergestellt werden!</h2></body></html>", "text/html", "UTF-8");
