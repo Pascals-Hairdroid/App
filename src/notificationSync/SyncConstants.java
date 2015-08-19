cd54f615241ffb5b108f2e438ba61f1ded87b811
oli9k9 <oli9k9@hotmail.com>
2015-08-19 11:12:12 +0200

    Wie gewünscht, Frequenz von SyncAdapter beschleunigt

--- src/notificationSync/SyncConstants.java
+++ src/notificationSync/SyncConstants.java
@@ -8,5 +8,5 @@ public class SyncConstants {
     public static final String AUTHTOKEN_TYPE_FULL_ACCESS = "Full access";
     public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an My Routes account";
     public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an My Routes account";
-    public static final long FREQ = 360; // Sync Frequenz in Minuten (360 = 6 Stunden)
+    public static final long FREQ = 10; // Sync Frequenz in Minuten (360 = 6 Stunden) Vorübergehend auf 10 geändert!!
 }
